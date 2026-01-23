ba0f8e3411f272eeb5a5a8d7cad3a85648487176
Gunanarpavi <gunanarpavii@gmail.com>
2026-01-23 08:52:37 +0000

    Added java program for primitive datatypes

---
+++ basics/Datatype.java
@@ -0,0 +1,23 @@
+class Datatype{
+    public static void main (String[]args){
+        byte b=10;
+        short s=100;
+        int i =1000;
+        long l=100000l;
+
+        float f=34.98f;
+        double d=45.9087f;
+
+        char c='A';
+        Boolean w=true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + w);
+    }
+}
