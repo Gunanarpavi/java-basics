7d2b2cafdee1ad972ccea5dbd0d07f3801d840d4
Gunanarpavi <gunanarpavii@gmail.com>
2026-01-24 07:00:17 +0000

    Added if_else program using default Default input

---
+++ conditional/simpleif/if_else/Default_input.java
@@ -0,0 +1,10 @@
+class Default_input{
+    public static void main(String[] args) {
+        int a =15;
+        if(a>10)
+        System.out.println(a+ "is greater than 10");
+        else
+         System.out.println(a+ "is lesser than 10");
+
+    }
+}
